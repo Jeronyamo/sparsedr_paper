3a7ba762845dc31f2deee0e141701a53fab464d5
Jeronyamo <85033670+Jeronyamo@users.noreply.github.com>
2026-02-13 13:07:43 +0300

    Time bar plot (1024x1024, without baseline)

--- plots.py
+++ plots.py
@@ -3,12 +3,22 @@ import numpy as np
 
 # Print barplot with iteration times
 grid_equivalents = ('32³', '64³', '128³', '256³', '512³')
+
+image_size = "(1024x1024)"
 stage_times = {
-    'Rendering': np.array([180, 180, 340, 480, 760]),
+    'Rendering': np.array([447, 501, 584, 838, 1693]),
     'Regularization': np.array([1, 1, 10, 10, 100]),
-    'Redistancing': np.array([1, 3, 12, 70, 110]),
+    'Optimizer': np.array([3, 3, 4, 10, 40]),
+    'Redistancing': np.array([9, 12, 24, 79, 318]),
 }
-stage_colors = [ "orangered", "royalblue", "springgreen" ]
+# image_size = "(512x512)"
+# stage_times = {
+#     'Rendering': np.array([180, 180, 340, 480, 760]),
+#     'Regularization': np.array([1, 1, 10, 10, 100]),
+#     'Optimizer': np.array([3, 3, 4, 10, 40]),
+#     'Redistancing': np.array([9, 12, 24, 79, 318]),
+# }
+stage_colors = [ "royalblue", "limegreen", "gold", "tomato" ]
 width = 0.6  # the width of the bars: can also be len(x) sequence
 
 
@@ -21,8 +31,8 @@ for i, stage in enumerate(stage_times.items()):
 
     # ax.bar_label(p, label_type='center')
 
-ax.set_title('Average time of 1 iteration')
-ax.set_xlabel("Grid equivalent")
+ax.set_title('Average time of 1 iteration ' + image_size)
+ax.set_xlabel("Equivalent grid size")
 ax.set_ylabel("Time, ms")
 ax.legend()
 plt.savefig("figures/time_1iter.png")
